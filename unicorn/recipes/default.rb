f886f4f168974821a5bfb493397650c8da2297fc
Geoff Hayes <hayesgm@gmail.com>
2013-03-20 16:01:27 -0700

    updates for log

--- unicorn/recipes/default.rb
+++ unicorn/recipes/default.rb
@@ -1,3 +1,5 @@
+Chef::Log.info "Running Shadow Version of Unicorn Recipe!"
+
 ruby_block "ensure only our unicorn version is installed by deinstalling any other version" do
   block do
     ensure_only_gem_version('unicorn', node[:unicorn][:version])
