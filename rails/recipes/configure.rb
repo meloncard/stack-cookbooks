88d2155df51ee76c86fa0c6269472721f83aa047
Geoff Hayes <hayesgm@gmail.com>
2013-03-21 03:12:47 -0700

    add some basic ymls

--- rails/recipes/configure.rb
+++ rails/recipes/configure.rb
@@ -26,6 +26,56 @@ node[:deploy].each do |application, deploy|
     end
   end
 
+  # We're going to add a Beanstalk.yml config - HGH
+  template "#{deploy[:deploy_to]}/shared/config/beanstalk.yml" do
+    source "beanstalk.yml.erb"
+    cookbook 'rails'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables(:queue => { :ip => "127.0.0.1", :port => 11300 },
+              :work => { :ip => "127.0.0.1", :port => 11301 }, 
+              :environment => deploy[:rails_env])
+
+    notifies :run, resources(:execute => "restart Rails app #{application}")
+
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
+  # We're going to add a Merchant.yml config - HGH
+  template "#{deploy[:deploy_to]}/shared/config/merchant.yml" do
+    source "merchant.yml.erb"
+    cookbook 'rails'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables(:merchant_api_key => 'xxx', :environment => deploy[:rails_env])
+    
+    notifies :run, resources(:execute => "restart Rails app #{application}")
+
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
+  # Then redis.yml - HGH
+  template "#{deploy[:deploy_to]}/shared/config/redis.yml" do
+    source "redis.yml.erb"
+    cookbook 'rails'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables(:session => { :ip => "127.0.0.1", :port => 6379 }, :environment => deploy[:rails_env])
+    
+    notifies :run, resources(:execute => "restart Rails app #{application}")
+
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
   template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
     source "memcached.yml.erb"
     cookbook 'rails'
