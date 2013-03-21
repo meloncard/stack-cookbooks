fdc47f31b538952d4ee78af720954e8baab4b2bc
Geoff Hayes <hayesgm@gmail.com>
2013-03-21 03:46:36 -0700

    make memcached optional

--- rails/recipes/configure.rb
+++ rails/recipes/configure.rb
@@ -76,18 +76,21 @@ node[:deploy].each do |application, deploy|
     end
   end
 
-  template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
-    source "memcached.yml.erb"
-    cookbook 'rails'
-    mode "0660"
-    group deploy[:group]
-    owner deploy[:user]
-    variables(:memcached => deploy[:memcached], :environment => deploy[:rails_env])
+  if deploy[:memcached] # This will be optional - HGH
+    template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
+      source "memcached.yml.erb"
+      cookbook 'rails'
+      mode "0660"
+      group deploy[:group]
+      owner deploy[:user]
+      variables(:memcached => deploy[:memcached], :environment => deploy[:rails_env])
 
-    notifies :run, resources(:execute => "restart Rails app #{application}")
+      notifies :run, resources(:execute => "restart Rails app #{application}")
 
-    only_if do
-      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+      only_if do
+        File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+      end
     end
   end
+
 end
