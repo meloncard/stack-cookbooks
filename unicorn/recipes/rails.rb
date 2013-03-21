f3bbf373de3f65d5d906bf931b2c70896ea0ed00
Geoff Hayes <hayesgm@gmail.com>
2013-03-20 19:11:55 -0700

    add a debug message

--- unicorn/recipes/rails.rb
+++ unicorn/recipes/rails.rb
@@ -5,6 +5,8 @@ end
 include_recipe "nginx"
 include_recipe "unicorn"
 
+Chef::Log.debug("Deploy: #{ node[:deploy].map { |l| l.inspect } }")
+
 # setup Unicorn service per app
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'rails'
