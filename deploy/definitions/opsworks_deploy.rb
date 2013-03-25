58b7f558d0703590020086a9ea4999c64a8df0fc
Geoff Hayes <hayesgm@gmail.com>
2013-03-25 14:09:57 -0700

    remove sym link

--- deploy/definitions/opsworks_deploy.rb
+++ deploy/definitions/opsworks_deploy.rb
@@ -96,24 +96,8 @@ define :opsworks_deploy do
         if deploy[:application_type] == 'rails'
           Chef::Log.info("Symlinking #{release_path}/public/assets to #{new_resource.deploy_to}/shared/assets")
           
-          link "#{release_path}/public/assets" do
-            to "#{new_resource.deploy_to}/shared/assets"
-          end
-           
           rails_env = new_resource.environment["RAILS_ENV"]
           Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
-          
-          execute "show beanstalk" do
-            cwd release_path
-            command "cat config/beanstalk.yml"
-            environment "RAILS_ENV" => rails_env
-          end
-
-          execute "show env" do
-            cwd release_path
-            command "echo $RAILS_ENV"
-            environment "RAILS_ENV" => rails_env
-          end
           
           execute "rake assets:precompile" do
             cwd release_path
