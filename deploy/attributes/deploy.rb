54d7b0a7d7ffe9c2ee4734d92c5115e6c29d0ffa
Geoff Hayes <hayesgm@gmail.com>
2013-08-10 13:39:25 -0700

    add seeding into migration command

--- deploy/attributes/deploy.rb
+++ deploy/attributes/deploy.rb
@@ -34,6 +34,7 @@ node[:deploy].each do |application, deploy|
 
   default[:deploy][application][:migrate] = false
 
+  # - Add seed post migration -HGH
   if node[:deploy][application][:auto_bundle_on_deploy]
     default[:deploy][application][:migrate_command] = "if [ -f Gemfile ]; then echo 'OpsWorks: Gemfile found - running migration with bundle exec' && /usr/local/bin/bundle exec #{node[:deploy][application][:rake]} db:migrate && /usr/local/bin/bundle exec #{node[:deploy][application][:rake]} db:seed; else echo 'OpsWorks: no Gemfile - running plain migrations' && #{node[:deploy][application][:rake]} db:migrate && #{node[:deploy][application][:rake]} db:seed; fi"
   else
