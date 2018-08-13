58e4c81345fa7c1057bfefa176e89d4aa5aa4079
Matt Van <m@mattvv.com>
2018-08-13 17:37:14 -0400

    Remove redis config

--- rails/recipes/configure.rb
+++ rails/recipes/configure.rb
@@ -92,15 +92,15 @@ node[:deploy].each do |application, deploy|
     end
   end
 
-  redis_server = node[:opsworks][:layers][:redis][:instances].keys.first rescue nil
-  redis_client = nil
+#   redis_server = node[:opsworks][:layers][:redis][:instances].keys.first rescue nil
+#   redis_client = nil
 
-  # Require a redis server or config
-  if redis_server
-    redis_client = node[:opsworks][:layers][:redis][:instances][redis_server][:private_dns_name]
-  else
-    redis_client = deploy[:redis][:client]
-  end
+#   # Require a redis server or config
+#   if redis_server
+#     redis_client = node[:opsworks][:layers][:redis][:instances][redis_server][:private_dns_name]
+#   else
+#     redis_client = deploy[:redis][:client]
+#   end
 
   # Then redis.yml - HGH
   template "#{deploy[:deploy_to]}/shared/config/redis.yml" do
